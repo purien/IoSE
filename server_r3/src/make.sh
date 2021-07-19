1f370eab22e824d19d2a44f5ed9b4a5224f6f75f
Pascal Urien <pascal.urien@gmail.com>
2021-07-19 10:44:05 +0200

    Update make.sh

--- server_r3/src/make.sh
+++ server_r3/src/make.sh
@@ -4,7 +4,7 @@ LIB= "-L/usr/lib"
 # https://transang.me/library-path-in-gcc/
 rm *.o
 rm racs2
-# gcc $INCLUDE -O2 -Wall  main.c -o main.o mutuex.c -o mutuex.o  pcsc.c -o pcsc.o pcscemulator.c -o pcscemulator.c  grid.c -o grid.o serverk.c -o serverk.o windowglue.c -o windowglue.o
+
 gcc -c -O2 -Wall ./main.c           -o  ./main.o $INCLUDE
 gcc -c -O2 -Wall ./reentrant2.c     -o ./reentrant2.o $INCLUDE
 gcc -c -O2 -Wall ./common2.c        -o ./common2.o $INCLUDE
@@ -17,8 +17,3 @@ gcc -c -O2 -Wall ./server6.c        -o ./server6.o    $INCLUDE
 gcc -c -O2 -Wall ./windowglue.c     -o ./windowglue.o $INCLUDE
 
 gcc -o racs2 ./main.o  ./pcsc.o ./atr.o ./pcscemulator.o ./grid.o ./serverk.o ./windowglue.o  ./common2.o  ./reentrant2.o ./server6.o  $LIB -lpcsclite -lpthread -lssl -lcrypto
-
-#gcc -o racs2 -Wall -O2  ./main.c   ./common2.c  ./reentrant2.c ./server6.c   ./pcsc.c ./atr.c ./pcscemulator.c ./grid.c ./serverk.c  ./windowglue.c  -I/usr/include/PCSC -I.  -L/usr/lib -lpcsclite -lpthread -lssl -lcrypto
-
-
-
